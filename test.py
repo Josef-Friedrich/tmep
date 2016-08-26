7a3c8bbf20da572bed8ede8bcd4c76d131af9dae
Josef Friedrich <josef@friedrich.rocks>
2016-08-26 20:20:32 +0200

    Add unit test

--- test.py
+++ test.py
@@ -1,13 +1,23 @@
 #! /usr/bin/env python
 
+import unittest
 import path_macrotemplate as pmt
 
 template = '${lastname}; ${prename}'
 
-values = {'prename': 'Wolfgang Amadeus', 'lastname': 'Mozart'}
+values = {
+    'prename': 'Wolfgang Amadeus',
+    'lastname': 'Mozart'
+}
 
 t = pmt.Template(template)
 f = pmt.Functions(values)
 out = t.substitute(values, f.functions)
 
-print(out)
+class TestStringMethods(unittest.TestCase):
+
+    def test_values(self):
+        self.assertEqual(out, 'Mozart; Wolfgang Amadeus')
+
+if __name__ == '__main__':
+    unittest.main()
