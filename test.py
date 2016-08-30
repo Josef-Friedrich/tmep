a72513b883157a8aaeee2c0eb37f7781dce1cd98
Josef Friedrich <josef@friedrich.rocks>
2016-08-30 12:03:01 +0200

    Convert spaces to tabs

--- test.py
+++ test.py
@@ -5,36 +5,36 @@ import tmep
 
 class TestClasses(unittest.TestCase):
 
-    def setUp(self):
-        template = '${lastname}; ${prename}'
+	def setUp(self):
+		template = '${lastname}; ${prename}'
 
-        values = {
-            'prename': 'Wolfgang Amadeus',
-            'lastname': 'Mozart'
-        }
+		values = {
+			'prename': 'Wolfgang Amadeus',
+			'lastname': 'Mozart'
+		}
 
-        t = tmep.Template(template)
-        f = tmep.Functions(values)
-        self.out = t.substitute(values, f.functions)
+		t = tmep.Template(template)
+		f = tmep.Functions(values)
+		self.out = t.substitute(values, f.functions)
 
-    def test_values(self):
-        self.assertEqual(self.out, 'Mozart; Wolfgang Amadeus')
+	def test_values(self):
+		self.assertEqual(self.out, 'Mozart; Wolfgang Amadeus')
 
 class TestParseDef(unittest.TestCase):
 
-    def setUp(self):
-        template = '${lastname}; ${prename}'
+	def setUp(self):
+		template = '${lastname}; ${prename}'
 
-        values = {
-            'prename': 'Wolfgang Amadeus',
-            'lastname': 'Mozart'
-        }
+		values = {
+			'prename': 'Wolfgang Amadeus',
+			'lastname': 'Mozart'
+		}
 
-        self.out = tmep.parse(template, values)
+		self.out = tmep.parse(template, values)
 
-    def test_values(self):
-        self.assertEqual(self.out, 'Mozart; Wolfgang Amadeus')
+	def test_values(self):
+		self.assertEqual(self.out, 'Mozart; Wolfgang Amadeus')
 
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
