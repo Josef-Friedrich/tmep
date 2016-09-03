2f8f00fef7925f2cbb7124c588394a5b6d2648e0
Josef Friedrich <josef@friedrich.rocks>
2016-09-03 19:56:39 +0200

    Fix indentation

--- setup.py
+++ setup.py
@@ -2,24 +2,24 @@ import os
 from setuptools import setup
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+	return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = 'tmep',
-    version = '0.0.2',
-    author = 'Josef Friedrich',
-    author_email = 'josef@friedrich.rocks',
-    description = ('Template and Macros Expansion for Path names.'),
-    license = 'MIT',
-    packages = ['tmep'],
-    url = 'https://github.com/Josef-Friedrich/tmep',
-    install_requires = [
-        'unidecode', 'six',
-    ],
-    long_description=read('README.rst'),
-    classifiers = [
-        'Development Status :: 3 - Alpha',
-        'Topic :: Utilities',
-    ],
-    zip_safe=False,
+	name = 'tmep',
+	version = '0.0.2',
+	author = 'Josef Friedrich',
+	author_email = 'josef@friedrich.rocks',
+	description = ('Template and Macros Expansion for Path names.'),
+	license = 'MIT',
+	packages = ['tmep'],
+	url = 'https://github.com/Josef-Friedrich/tmep',
+	install_requires = [
+		'unidecode', 'six',
+	],
+	long_description=read('README.rst'),
+	classifiers = [
+		'Development Status :: 3 - Alpha',
+		'Topic :: Utilities',
+	],
+	zip_safe=False,
 )
