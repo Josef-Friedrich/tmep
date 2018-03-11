4acb74fd9ffe9a2119eade94c94d6d63064e5d75
Josef Friedrich <josef@friedrich.rocks>
2018-03-11 10:53:57 +0100

    Test all format functions

--- test/test_format.py
+++ test/test_format.py
@@ -1,18 +1,80 @@
 # -*- coding: utf-8 -*-
 import unittest
-from tmep.format import alphanum, asciify, delchars
+from tmep.format import \
+    alpha, \
+    alphanum, \
+    asciify, \
+    delchars, \
+    deldupchars, \
+    first, \
+    initial, \
+    left, \
+    lower, \
+    num, \
+    replchars, \
+    right, \
+    sanitize, \
+    shorten, \
+    time, \
+    title, \
+    upper
 
 
 class TestFormatFunctions(unittest.TestCase):
 
+    def test_alpha(self):
+        self.assertTrue(callable(alpha))
+
     def test_alphanum(self):
         self.assertEqual(alphanum('a-b'), 'a b')
+        self.assertTrue(callable(alphanum))
 
     def test_asciify(self):
         self.assertEqual(asciify('ä'), 'ae')
+        self.assertTrue(callable(asciify))
 
     def test_delchars(self):
         self.assertEqual(delchars('a-b', '-'), 'ab')
+        self.assertTrue(callable(delchars))
+
+    def test_deldupchars(self):
+        self.assertTrue(callable(deldupchars))
+
+    def test_first(self):
+        self.assertTrue(callable(first))
+
+    def test_initial(self):
+        self.assertTrue(callable(initial))
+
+    def test_left(self):
+        self.assertTrue(callable(left))
+
+    def test_lower(self):
+        self.assertTrue(callable(lower))
+
+    def test_num(self):
+        self.assertTrue(callable(num))
+
+    def test_replchars(self):
+        self.assertTrue(callable(replchars))
+
+    def test_right(self):
+        self.assertTrue(callable(right))
+
+    def test_sanitize(self):
+        self.assertTrue(callable(sanitize))
+
+    def test_shorten(self):
+        self.assertTrue(callable(shorten))
+
+    def test_time(self):
+        self.assertTrue(callable(time))
+
+    def test_title(self):
+        self.assertTrue(callable(title))
+
+    def test_upper(self):
+        self.assertTrue(callable(upper))
 
 
 if __name__ == '__main__':
